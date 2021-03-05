4ecabd78a88efd9a6548d92aa900aee2c46d6431
yusuf b <ykbilgin61@gmail.com>
2021-03-05 22:35:35 +0100

    post/admin.py
    Signed-off-by: yusuf b <ykbilgin61@gmail.com>

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Post, Comment
 
-# Register your models here.
+
+class CommentInline(admin.StackedInline):
+    verbose_name = "Yorum"
+    verbose_name_plural = "Yorumlar"
+    model = Comment
+    extra = 1
+
+
+class PostAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (
+            'Gönderi detayları',
+            {
+                'fields': ['user', 'post_image', 'description']
+            }
+        )
+    ]
+    inlines = [CommentInline]
+
+
+admin.site.register(Post, PostAdmin)
