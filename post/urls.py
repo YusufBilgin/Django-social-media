f4eda4588de8d5fc790bb6bd47ec1a144064641d
yusuf b <ykbilgin61@gmail.com>
2021-03-05 22:25:14 +0100

    update post/urls.py
    Signed-off-by: yusuf b <ykbilgin61@gmail.com>

--- post/urls.py
+++ post/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('beğen/<int:post_id>/', views.like, name = "like"),
     path('arşive-kaydet/<int:post_id>/', views.save_post_to_archive, name = "savePostToArchive"),
     path('yorum-yap/<int:id>/<str:redirectTo>/', views.addComment, name = "addComment"),
-    path('kategorileri-ayarla/<int:id>/', views.set_categories, name = "setCategories"),
-    path('admin-paneli/', views.admin_pamel_to_confirm_pictures, name = "adminPanel"),
     # path('sil/<int:id>/', views.delete_post, name = "deletePost"),
 ]
