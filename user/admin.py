b834a5f4c9406d28e17dd1567bc1d2efd74407e1
yusuf b <ykbilgin61@gmail.com>
2021-03-07 16:39:04 +0100

    added user/admin.py
    Signed-off-by: yusuf b <ykbilgin61@gmail.com>

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Profile, UserFollowing
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_filter = ['user__date_joined']
+    # Evet nasil calisti hic bir fikrim yok ama iyi bu kalsin.
+    search_fields = ['user__username']
+
+
+class UserFollowingAdmin(admin.ModelAdmin):
+    list_filter = ['created']
+    search_fields = ['user_id__username']
+
+
+admin.site.register(Profile, ProfileAdmin)
+admin.site.register(UserFollowing, UserFollowingAdmin)
